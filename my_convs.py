81ee3033ec4e508f9571163d62515143ee9baccf
Chendi Qian <1049653012@qq.com>
2023-11-06 13:58:16 +0100

    push

--- my_convs.py
+++ my_convs.py
@@ -2,27 +2,18 @@ from typing import Optional, Union
 
 import torch
 import torch.nn.functional as F
-from torch import Tensor
-from torch.nn import Linear, ReLU, GELU, Sequential, BatchNorm1d as BN
 from torch_geometric.nn import MessagePassing, MLP
-from torch_geometric.typing import Adj, OptTensor
-from torch_geometric.utils import degree
-from ogb.graphproppred.mol_encoder import AtomEncoder, BondEncoder
+from ogb.graphproppred.mol_encoder import BondEncoder, AtomEncoder
 
 
 class GINEConv(MessagePassing):
     def __init__(self, emb_dim: int = 64,
                  mlp: Optional[Union[MLP, torch.nn.Sequential]] = None,
-                 bond_encoder: Optional[Union[MLP, torch.nn.Sequential]] = None,
-                 dropout: float = 0.):
+                 bond_encoder: Optional[Union[MLP, torch.nn.Sequential]] = None):
 
         super(GINEConv, self).__init__(aggr="add")
 
-        if mlp is not None:
-            self.mlp = mlp
-        else:
-            self.mlp = MLP([emb_dim, emb_dim, emb_dim], norm='batchnorm', dropout=dropout)
-
+        self.mlp = mlp
         self.eps = torch.nn.Parameter(torch.Tensor([0.]))
 
         if bond_encoder is None:
@@ -31,10 +22,14 @@ class GINEConv(MessagePassing):
             self.bond_encoder = bond_encoder
 
     def forward(self, x, edge_index, edge_attr, edge_weight):
-        if edge_weight is not None and edge_weight.ndim < 2:
-            edge_weight = edge_weight[:, None]
+        repeats, nnodes, features = x.shape
+        if edge_attr is not None:
+            edge_embedding = self.bond_encoder(edge_attr)
+            edge_embedding = edge_embedding.repeat(repeats, 1, 1)
+        else:
+            edge_embedding = None
 
-        edge_embedding = self.bond_encoder(edge_attr) if edge_attr is not None else None
+        x = x.reshape(-1, features)
         out = self.mlp((1 + self.eps) * x + self.propagate(edge_index,
                                                            x=x,
                                                            edge_attr=edge_embedding,
@@ -47,6 +42,3 @@ class GINEConv(MessagePassing):
 
     def update(self, aggr_out):
         return aggr_out
-
-    def reset_parameters(self):
-        raise NotImplementedError
